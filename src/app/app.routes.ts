fe245d70508ec55d418b423d7b23c972c1fa84fe
taras.schevchuk <taras.schevchuk@amedial.com>
2025-02-19 10:32:16 +0100

    initial settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
+import {MovieListComponent} from './features/movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: '', component: MovieListComponent }],
+  },
+];
---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
